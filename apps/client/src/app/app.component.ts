2d6e7269c03cdf7eaeb2df192aaf9a25ab217ee9
Patrick Son <patrick.son@comcast.net>
2019-02-02 16:54:47 -0500

    Initialized client and server files

---
+++ apps/client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'character-suite-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'client';
+}
